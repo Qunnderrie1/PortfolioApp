41f1d190acec19bed9131df2b3d37e2b42766d8c
qunnderrie1 <qunnderrie@gmail.com>
2023-07-30 20:06:22 -0400

    made small changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     <div className='headerContainer container'>
 
         <h1 style={{fontWeight: "light"}}> Hello, I'm A <span>Full Stack Developer</span></h1>
-        <p>A passionate web developer that have experienced using frontend and backend technologies.</p>
+        <p>A passionate web developer that has experience using frontend and backend technologies.</p>
 
         <a href="Q_Developer_Resume.pdf" download>
         <button>Resume CV</button>
