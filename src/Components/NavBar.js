66a9ba3a7989b177fff64083d6fbd679921fe9de
qunnderrie1 <qunnderrie@gmail.com>
2024-01-17 11:53:10 -0500

    made changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -22,9 +22,6 @@ const NavBar = () => {
             <a class="nav-link" href="#portfolio">Portfolio</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#about">About</a>
-          </li>
-          <li class="nav-item">
             <a class="nav-link" href="#contact">Contact</a>
           </li>
         </ul>
